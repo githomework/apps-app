2d0f378c32e86b24d7e6b8b59dfd773a54066ef3
David Lam <david.lam@winc.com.au>
2020-09-03 22:59:41 +1000

    commit

--- app.go
+++ app.go
@@ -17,22 +17,20 @@ type AppType struct {
 	ConfigPath     string
 }
 
-
-func New() AppType{
-	var App AppType
+func (app *AppType) Setup() {
 	exe, _ := os.Executable()
 	// strange problem: if FolderAndSlash is used instead of f, the change in FolderAndSlash is not visible outside of this function.
 	f, n := filepath.Split(exe)
-	App.FolderAndSlash = f
-	App.NameWithExe = n
-	App.FilePath = App.FolderAndSlash + App.NameWithExe
-	App.Folder = App.FolderAndSlash[:len(App.FolderAndSlash)-1] // get rid of trailing backslash of folder
-	App.Name = App.NameWithExe
-	if len(App.Name) >= 4 && App.Name[len(App.Name)-4:] == ".exe" {
-		App.Name = App.Name[:len(App.Name)-4] // get rid of .exe of name
+	app.FolderAndSlash = f
+	app.NameWithExe = n
+	app.FilePath = app.FolderAndSlash + app.NameWithExe
+	app.Folder = app.FolderAndSlash[:len(app.FolderAndSlash)-1] // get rid of trailing backslash of folder
+	app.Name = app.NameWithExe
+	if len(app.Name) >= 4 && app.Name[len(app.Name)-4:] == ".exe" {
+		app.Name = app.Name[:len(app.Name)-4] // get rid of .exe of name
 	}
 
-	ff, err := os.OpenFile(App.FolderAndSlash+App.Name+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	ff, err := os.OpenFile(app.FolderAndSlash+app.Name+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening log file: %v", err)
 	}
@@ -46,11 +44,10 @@ func New() AppType{
 		ConfigPath string `toml:"config_file"`
 	}
 
-	if _, err := toml.DecodeFile(App.FolderAndSlash+"config.toml", &configFile); err != nil {
+	if _, err := toml.DecodeFile(app.FolderAndSlash+"config.toml", &configFile); err != nil {
 		log.Printf("%s", err)
 	}
 
-	App.ConfigPath = configFile.ConfigPath
-	return App
+	app.ConfigPath = configFile.ConfigPath
 
 }
