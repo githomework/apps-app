7c31d72a16adfa5fb4bdd7052587e7e803632629
David Lam <david.lam@winc.com.au>
2020-10-02 16:21:55 +1000

    added status update functions

--- status_update.go
+++ status_update.go
@@ -55,3 +55,48 @@ func UpdateStatusFinished7713(session string) {
 	io.Copy(ioutil.Discard, resp.Body)
 
 }
+
+func UpdateStatusBegan(session string, port string) {
+	var jsonStr strings.Builder
+	jsonStr.Grow(100)
+	jsonStr.WriteString(`{"session_id":"`)
+	jsonStr.WriteString(session)
+	jsonStr.WriteString(`","began":"`)
+	jsonStr.WriteString(time.Now().Format("2006-01-02 15:04:05.000"))
+	jsonStr.WriteString(`"}`)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:"+port+"/updateStatusBegan", strings.NewReader(jsonStr.String()))
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	io.Copy(ioutil.Discard, resp.Body)
+
+}
+
+func UpdateStatusFinished(session string, port string) {
+	var jsonStr strings.Builder
+	jsonStr.Grow(100)
+	jsonStr.WriteString(`{"session_id":"`)
+	jsonStr.WriteString(session)
+	jsonStr.WriteString(`","finished":"`)
+	jsonStr.WriteString(time.Now().Format("2006-01-02 15:04:05.000"))
+	jsonStr.WriteString(`"}`)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:"+port+"/updateStatusFinished", strings.NewReader(jsonStr.String()))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	io.Copy(ioutil.Discard, resp.Body)
+
+}
