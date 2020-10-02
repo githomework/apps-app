59d49e096eb494035aa9349cd5568ddfad831a49
David Lam <david.lam@winc.com.au>
2020-10-02 17:25:55 +1000

    ExtractSessoinInfo

--- app_test.go
+++ app_test.go
@@ -1,7 +1,29 @@
 package app
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestFolder(t *testing.T) {
+	"github.com/buger/jsonparser"
+)
 
+func TestParser(t *testing.T) {
+	data := []byte(`{"x":"xx","y":"yy"}`)
+	m := map[string]string{}
+	jsonparser.EachKey(data, func(i int, value []byte, vt jsonparser.ValueType, err error) {
+		if err != nil {
+			fmt.Println(err)
+		}
+		switch i {
+		case 0:
+			m["x"] = string(value)
+		case 1:
+			m["y"] = string(value)
+		}
+
+	}, []string{"x"}, []string{"y"})
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
--- http.go
+++ http.go
@@ -2,7 +2,11 @@ package app
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/buger/jsonparser"
 )
 
 func (a *AppType) PingResponse(w http.ResponseWriter, req *http.Request) {
@@ -12,4 +16,44 @@ func (a *AppType) PingResponse(w http.ResponseWriter, req *http.Request) {
 
 func (a *AppType) FolderResponse(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, a.Folder)
+}
+
+func ExtractSessionInfo(req *http.Request) map[string]string {
+	m := map[string]string{}
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println(err)
+		return m
+	}
+	jsonparser.EachKey(b, func(i int, value []byte, vt jsonparser.ValueType, err error) {
+		if err != nil {
+			return
+		}
+		switch i {
+		case 0:
+			m["session_id"] = string(value)
+		case 1:
+			m["began"] = string(value)
+		case 2:
+			m["finished"] = string(value)
+		case 3:
+			m["task_name"] = string(value)
+		case 4:
+			m["brcd"] = string(value)
+		case 5:
+			m["printer"] = string(value)
+		case 6:
+			m["ref"] = string(value)
+		case 7:
+			m["content"] = string(value)
+		}
+	}, []string{"session_id"},
+		[]string{"began"},
+		[]string{"finished"},
+		[]string{"task_name"},
+		[]string{"brcd"},
+		[]string{"printer"},
+		[]string{"ref"},
+		[]string{"content"})
+	return m
 }
