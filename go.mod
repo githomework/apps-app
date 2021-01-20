e1516a660d7c3f3a96e6d23ca09245530e2401bc
David Lam <david.lam@winc.com.au>
2021-01-20 11:37:55 +1100

    KV

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/githomework/apps-app
 
 go 1.14
 
-require github.com/BurntSushi/toml v0.3.1
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/buger/jsonparser v1.0.0
+)
---
+++ kv.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"database/sql"
+	"log"
+)
+
+func KVString(k string, db *sql.DB) (values []string) {
+	// assumes postgresql syntax and specific table
+	dbRows, err := db.Query("select string_value from common.kv where key = $1", k)
+	if err != nil {
+		log.Println("Error in KVString():", err)
+		return
+	}
+	for dbRows.Next() {
+		var s string
+		dbRows.Scan(&s)
+		values = append(values, s)
+	}
+	return
+}
+
+func KVFloat(k string, db *sql.DB) (values []float64) {
+	// assumes postgresql syntax and specific table
+	dbRows, err := db.Query("select string_value from common.kv where key = $1", k)
+	if err != nil {
+		log.Println("Error in KVFloat():", err)
+		return
+	}
+	for dbRows.Next() {
+		var f float64
+		dbRows.Scan(&f)
+		values = append(values, f)
+	}
+	return
+}
